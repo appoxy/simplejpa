fc4b17b9a1399843c97e3055a70f9508235a4a64
Aldrin Leal <aldrinleal@odeskps.com>
2012-01-15 11:59:51 -0300

    - Fixing @ManyToOne without cascade parameters

--- core/src/main/java/com/spaceprogram/simplejpa/operations/Save.java
+++ core/src/main/java/com/spaceprogram/simplejpa/operations/Save.java
@@ -41,9 +41,7 @@ import com.spaceprogram.simplejpa.LazyInterceptor;
 import com.spaceprogram.simplejpa.PersistentProperty;
 
 /**
- * User: treeder
- * Date: Apr 1, 2008
- * Time: 11:51:16 AM
+ * User: treeder Date: Apr 1, 2008 Time: 11:51:16 AM
  */
 public class Save implements Callable {
     private static Logger logger = Logger.getLogger(Save.class.getName());
@@ -58,13 +56,14 @@ public class Save implements Callable {
         this.o = o;
         long start = System.currentTimeMillis();
         id = prePersist(o);
-        if(logger.isLoggable(Level.FINE)) logger.fine("prePersist time=" + (System.currentTimeMillis() - start));
+        if (logger.isLoggable(Level.FINE))
+            logger.fine("prePersist time=" + (System.currentTimeMillis() - start));
 
     }
 
     /**
      * Checks that object is an entity and assigns an ID.
-     *
+     * 
      * @param o
      * @return
      */
@@ -75,14 +74,13 @@ public class Save implements Callable {
         if (id == null) {
             newObject = true;
             id = UUID.randomUUID().toString();
-//            System.out.println("new object, setting id");
+            // System.out.println("new object, setting id");
             AnnotationInfo ai = em.getFactory().getAnnotationManager().getAnnotationInfo(o);
             em.setFieldValue(o.getClass(), o, ai.getIdMethod(), Collections.singleton(id));
         }
         em.cachePut(id, o);
         return id;
     }
-
 
     public Object call() throws Exception {
         try {
@@ -95,7 +93,8 @@ public class Save implements Callable {
         return o;
     }
 
-    protected void persistOnly(Object o, String id) throws AmazonClientException, IllegalAccessException, InvocationTargetException, IOException {
+    protected void persistOnly(Object o, String id) throws AmazonClientException, IllegalAccessException,
+            InvocationTargetException, IOException {
         long start = System.currentTimeMillis();
         em.invokeEntityListener(o, newObject ? PrePersist.class : PreUpdate.class);
         AnnotationInfo ai = em.getFactory().getAnnotationManager().getAnnotationInfo(o);
@@ -103,14 +102,15 @@ public class Save implements Callable {
         UpdateCondition expected = null;
         PersistentProperty versionField = null;
         Integer nextVersion = -1;
-        
+
         String domainName;
         if (ai.getRootClass() != null) {
-        	domainName = em.getOrCreateDomain(ai.getRootClass());
+            domainName = em.getOrCreateDomain(ai.getRootClass());
         } else {
-        	domainName = em.getOrCreateDomain(o.getClass());
-        }        
-//        Item item = DomainHelper.findItemById(this.em.getSimpleDb(), domainName, id);
+            domainName = em.getOrCreateDomain(o.getClass());
+        }
+        // Item item = DomainHelper.findItemById(this.em.getSimpleDb(),
+        // domainName, id);
         // now set attributes
         List<ReplaceableAttribute> attsToPut = new ArrayList<ReplaceableAttribute>();
         List<Attribute> attsToDelete = new ArrayList<Attribute>();
@@ -121,18 +121,18 @@ public class Save implements Callable {
         LazyInterceptor interceptor = null;
         if (o instanceof Factory) {
             Factory factory = (Factory) o;
-            /*for (Callback callback2 : factory.getCallbacks()) {
-                if(logger.isLoggable(Level.FINER)) logger.finer("callback=" + callback2);
-                if (callback2 instanceof LazyInterceptor) {
-                    interceptor = (LazyInterceptor) callback2;
-                }
-            }*/
+            /*
+             * for (Callback callback2 : factory.getCallbacks()) {
+             * if(logger.isLoggable(Level.FINER)) logger.finer("callback=" +
+             * callback2); if (callback2 instanceof LazyInterceptor) {
+             * interceptor = (LazyInterceptor) callback2; } }
+             */
             interceptor = (LazyInterceptor) factory.getCallback(0);
         }
 
         for (PersistentProperty field : ai.getPersistentProperties()) {
-        	Object ob = field.getProperty(o);
-        	
+            Object ob = field.getProperty(o);
+
             String columnName = field.getColumnName();
             if (ob == null) {
                 attsToDelete.add(new Attribute(columnName, null));
@@ -141,21 +141,23 @@ public class Save implements Callable {
             if (field.isForeignKeyRelationship()) {
                 // store the id of this object
                 if (Collection.class.isAssignableFrom(field.getRawClass())) {
-                    for(Object each : (Collection)ob) {
+                    for (Object each : (Collection) ob) {
                         String id2 = em.getId(each);
                         attsToPut.add(new ReplaceableAttribute(columnName, id2, true));
                     }
                 } else {
                     String id2 = em.getId(ob);
                     attsToPut.add(new ReplaceableAttribute(columnName, id2, true));
-                    
+
                     /* check if we should persist this */
                     boolean persistRelationship = false;
                     ManyToOne a = field.getGetter().getAnnotation(ManyToOne.class);
-                    CascadeType[] cascadeType = a.cascade();
-                    for (CascadeType type : cascadeType) {
-                        if (CascadeType.ALL == type || CascadeType.PERSIST == type) {
-                            persistRelationship = true;
+                    if (a != null && null != a.cascade()) {
+                        CascadeType[] cascadeType = a.cascade();
+                        for (CascadeType type : cascadeType) {
+                            if (CascadeType.ALL == type || CascadeType.PERSIST == type) {
+                                persistRelationship = true;
+                            }
                         }
                     }
                     if (persistRelationship) {
@@ -167,14 +169,15 @@ public class Save implements Callable {
                 nextVersion = (1 + curVersion);
 
                 attsToPut.add(new ReplaceableAttribute(columnName, em.padOrConvertIfRequired(nextVersion), true));
-                
+
                 if (curVersion > 0)
                     expected = new UpdateCondition(columnName, em.padOrConvertIfRequired(curVersion), true);
-                
+
                 versionField = field;
             } else if (field.isInverseRelationship()) {
                 // FORCING BI-DIRECTIONAL RIGHT NOW SO JUST IGNORE
-                // ... except for cascading persistence down to all items in the OneToMany collection
+                // ... except for cascading persistence down to all items in the
+                // OneToMany collection
                 /* check if we should persist this */
                 boolean persistRelationship = false;
                 OneToMany a = field.getGetter().getAnnotation(OneToMany.class);
@@ -186,23 +189,25 @@ public class Save implements Callable {
                 }
                 if (persistRelationship) {
                     if (ob instanceof Collection) {
-                        // it's OneToMany, so this should always be the case, shouldn't it?
+                        // it's OneToMany, so this should always be the case,
+                        // shouldn't it?
                         for (Object _item : (Collection) ob) {
                             // persist each item in the collection
                             em.persist(_item);
                         }
                     }
                 }
-                
+
             } else if (field.isLob()) {
                 // store in s3
                 AmazonS3 s3 = null;
-                // todo: need to make sure we only store to S3 if it's changed, too slow.
+                // todo: need to make sure we only store to S3 if it's changed,
+                // too slow.
                 logger.fine("putting lob to s3");
                 long start3 = System.currentTimeMillis();
                 s3 = em.getS3Service();
-                String bucketName = em.getS3BucketName(); 
-                String s3ObjectId = id +"-"+ field.getFieldName();
+                String bucketName = em.getS3BucketName();
+                String s3ObjectId = id + "-" + field.getFieldName();
 
                 ByteArrayOutputStream bos = new ByteArrayOutputStream();
                 ObjectOutputStream out = new ObjectOutputStream(bos);
@@ -210,30 +215,28 @@ public class Save implements Callable {
                 byte[] contentBytes = bos.toByteArray();
                 out.close();
                 InputStream input = new ByteArrayInputStream(contentBytes);
-                
+
                 s3.putObject(bucketName, s3ObjectId, input, null);
-                
+
                 em.statsS3Put(System.currentTimeMillis() - start3);
                 logger.finer("setting lobkeyattribute=" + columnName + " - " + s3ObjectId);
                 attsToPut.add(new ReplaceableAttribute(columnName, s3ObjectId, true));
             } else if (field.getEnumType() != null) {
                 String toSet = getEnumValue(field, o);
                 attsToPut.add(new ReplaceableAttribute(columnName, toSet, true));
-            }
-            else if(field.isId())
-            {
-            	continue;
-            }
-            else if(Collection.class.isInstance(ob)) {
-                for(Object each : ((Collection)ob)) {
+            } else if (field.isId()) {
+                continue;
+            } else if (Collection.class.isInstance(ob)) {
+                for (Object each : ((Collection) ob)) {
                     String toSet = each != null ? em.padOrConvertIfRequired(each) : "";
-                    // todo: throw an exception if this is going to exceed maximum size, suggest using @Lob
+                    // todo: throw an exception if this is going to exceed
+                    // maximum size, suggest using @Lob
                     attsToPut.add(new ReplaceableAttribute(columnName, toSet, true));
                 }
-            }
-            else {
+            } else {
                 String toSet = ob != null ? em.padOrConvertIfRequired(ob) : "";
-                // todo: throw an exception if this is going to exceed maximum size, suggest using @Lob
+                // todo: throw an exception if this is going to exceed maximum
+                // size, suggest using @Lob
                 attsToPut.add(new ReplaceableAttribute(columnName, toSet, true));
             }
         }
@@ -241,25 +244,25 @@ public class Save implements Callable {
         // Now finally send it for storage (If have attributes to add)
         long start2 = System.currentTimeMillis();
         long duration2;
-        if (!attsToPut.isEmpty()) {        
-            this.em.getSimpleDb().putAttributes(new PutAttributesRequest()
-               .withDomainName(domainName)
-               .withItemName(id)
-               .withAttributes(attsToPut).withExpected(expected));
+        if (!attsToPut.isEmpty()) {
+            this.em.getSimpleDb().putAttributes(
+                    new PutAttributesRequest().withDomainName(domainName).withItemName(id).withAttributes(attsToPut)
+                            .withExpected(expected));
             duration2 = System.currentTimeMillis() - start2;
-            if(logger.isLoggable(Level.FINE))logger.fine("putAttributes time=" + (duration2));
+            if (logger.isLoggable(Level.FINE))
+                logger.fine("putAttributes time=" + (duration2));
             em.statsAttsPut(attsToPut.size(), duration2);
-            
+
             if (null != versionField)
                 versionField.setProperty(o, nextVersion);
         }
 
         /*
-         Check for nulled attributes so we can send a delete call.
-        Don't delete attributes if this is a new object
-        AND don't delete atts if it's not dirty
-        AND don't delete if no nulls were set (nulledField on LazyInterceptor)
-        */
+         * Check for nulled attributes so we can send a delete call. Don't
+         * delete attributes if this is a new object AND don't delete atts if
+         * it's not dirty AND don't delete if no nulls were set (nulledField on
+         * LazyInterceptor)
+         */
         if (interceptor != null) {
             if (interceptor.getNulledFields() != null && interceptor.getNulledFields().size() > 0) {
                 List<Attribute> attsToDelete2 = new ArrayList<Attribute>();
@@ -268,12 +271,11 @@ public class Save implements Callable {
                     attsToDelete2.add(new Attribute(columnName, null));
                 }
                 start2 = System.currentTimeMillis();
-                this.em.getSimpleDb().deleteAttributes(new DeleteAttributesRequest()
-                	.withDomainName(domainName)
-                	.withItemName(id)
-                	.withAttributes(attsToDelete2));
-                
-                // todo: what about lobs?  need to delete from s3
+                this.em.getSimpleDb().deleteAttributes(
+                        new DeleteAttributesRequest().withDomainName(domainName).withItemName(id)
+                                .withAttributes(attsToDelete2));
+
+                // todo: what about lobs? need to delete from s3
                 duration2 = System.currentTimeMillis() - start2;
                 logger.fine("deleteAttributes time=" + (duration2));
                 em.statsAttsDeleted(attsToDelete2.size(), duration2);
@@ -284,14 +286,14 @@ public class Save implements Callable {
             if (!newObject && attsToDelete.size() > 0) {
                 // not enhanced, but still have to deal with deleted attributes
                 start2 = System.currentTimeMillis();
-//                for (ItemAttribute itemAttribute : attsToDelete) {
-//                    System.out.println("itemAttr=" + itemAttribute.getName() + ": " + itemAttribute.getValue());
-//                }
-                this.em.getSimpleDb().deleteAttributes(new DeleteAttributesRequest()
-	            	.withDomainName(domainName)
-	            	.withItemName(id)
-	            	.withAttributes(attsToDelete));
-                // todo: what about lobs?  need to delete from s3
+                // for (ItemAttribute itemAttribute : attsToDelete) {
+                // System.out.println("itemAttr=" + itemAttribute.getName() +
+                // ": " + itemAttribute.getValue());
+                // }
+                this.em.getSimpleDb().deleteAttributes(
+                        new DeleteAttributesRequest().withDomainName(domainName).withItemName(id)
+                                .withAttributes(attsToDelete));
+                // todo: what about lobs? need to delete from s3
                 duration2 = System.currentTimeMillis() - start2;
                 logger.fine("deleteAttributes time=" + (duration2));
                 em.statsAttsDeleted(attsToDelete.size(), duration2);
@@ -302,7 +304,8 @@ public class Save implements Callable {
             interceptor.reset();
         }
         em.invokeEntityListener(o, newObject ? PostPersist.class : PostUpdate.class);
-        if(logger.isLoggable(Level.FINE)) logger.fine("persistOnly time=" + (System.currentTimeMillis() - start));
+        if (logger.isLoggable(Level.FINE))
+            logger.fine("persistOnly time=" + (System.currentTimeMillis() - start));
     }
 
     static String getEnumValue(PersistentProperty field, Object ob) {
@@ -328,6 +331,5 @@ public class Save implements Callable {
         }
         return toSet;
     }
-
 
 }
